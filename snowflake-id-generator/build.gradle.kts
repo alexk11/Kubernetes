780f66722b06a3f3b4901e71b0aa73edb4febec6
alexk11 <akryuchkov1@gmail.com>
2026-02-17 22:37:21 +0300

    docker multi-stage nextid app

---
+++ snowflake-id-generator/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.4.6"
+	id("org.jlleitschuh.gradle.ktlint") version libs.versions.gradle.ktlint
+	id("io.gitlab.arturbosch.detekt") version libs.versions.gradle.detekt
+	id("java")
+	kotlin("jvm") version libs.versions.kotlin
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+group = "snowflake"
+version = "0.0.1-SNAPSHOT-plain"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation(libs.kotlin.stdlib)
+	implementation(libs.kotlinx.coroutines)
+	implementation(libs.springframework.boot.starter.web)
+	implementation(libs.springframework.boot.starter.actuator)
+	testImplementation(libs.junit.jupiter)
+	testImplementation(libs.assertj.core)
+	testImplementation(libs.kotlinx.coroutines.test)
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs += "-Xjsr305=strict"
+		jvmTarget = "21"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ snowflake-id-generator/src/main/kotlin/snowflakegenerator/SnowflakeApplication.kt
@@ -0,0 +1,13 @@
+package snowflakegenerator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+@EnableConfigurationProperties
+open class SnowflakeApplication
+
+fun main(vararg args: String) {
+	runApplication<SnowflakeApplication>(*args)
+}
